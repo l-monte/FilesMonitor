a3ac88c599ced081fb0be292f7f7fa1e368bf466
Lukasz Manterys <l.manterys@samsung.com>
2019-10-18 15:00:54 +0200

    Reading files, part 1.

--- src/FilesReader/FileReader.cpp
+++ src/FilesReader/FileReader.cpp
@@ -1,6 +1,37 @@
 #include "FileReader.h"
+#include <QTimer>
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
 
-FileReader::FileReader(const QString& rootDir) : _rootFile(rootDir)
+FileReader::FileReader(const QString& rootDir) :
+    _rootFile(rootDir),
+    _scanningFileTimer(new QTimer(this)),
+    _filesMap()
 {
+    _scanningFileTimer->setSingleShot(false);
+    _scanningFileTimer->setInterval(10000);
+    _scanningFileTimer->start();                // TODO przenieść do funkcji start()
 
+    connect(_scanningFileTimer, &QTimer::timeout, this, &FileReader::scanFiles);
+}
+
+void FileReader::scanFiles()
+{
+    qDebug() << "LOG: FileReader.cpp : scanFiles() - start function";
+    QDir directory(QFileInfo(_rootFile.fileName()).absoluteFilePath());
+    QStringList files = directory.entryList(QDir::Files);
+
+    if (files.size() > 0)               // TODO
+        qDebug() << "\nLista plikow:";
+
+    foreach(QString fileName, files) {
+        _filesMap.insert(fileName, true);
+        qDebug() << "Plik: " << fileName;
+    }
+}
+
+FileReader::~FileReader()
+{
+    delete _scanningFileTimer;
 }
--- src/FilesReader/FileReader.h
+++ src/FilesReader/FileReader.h
@@ -2,14 +2,25 @@
 #define FILEREADER_H
 #include <QString>
 #include <QFile>
+#include <QHash>
 
-class FileReader
+class QTimer;
+
+class FileReader : public QObject
 {
+    Q_OBJECT
 public:
     FileReader(const QString& rootDir);
+    virtual ~FileReader() ;
+
+public slots:
+    void scanFiles();
 
 private:
     QFile _rootFile;
+
+    QTimer* _scanningFileTimer;
+    QHash<QString, bool> _filesMap;
 };
 
 #endif // FILEREADER_H
